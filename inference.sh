4ba273082509ef5cb27e4ee1f63156a94da151ad
danjuan-77 <youngertutu@gmail.com>
2025-04-23 19:59:57 +0800

    add infer test

---
+++ inference.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Text & Image Understanding
+python3 inference/infer.py --model_path "/share/nlp/tuwenming/models/THUdyh/Ola-7b" \
+    --image_path "./data/image.png" \
+    --text "Describe this image."
+
+
+# Text & Video Understanding
+python3 inference/infer.py --model_path "/share/nlp/tuwenming/models/THUdyh/Ola-7b" \
+    --video_path "./data/test.mp4"\
+    --text "Describe this video."
+
+
+# Text & Audio Understanding
+python3 inference/infer.py --model_path "/share/nlp/tuwenming/models/THUdyh/Ola-7b" \
+    --audio_path "./data/sample.wav" \
+    --text "Describe this audio."
+
+# Audio & Image Understanding
+python3 inference/infer.py --model_path "/share/nlp/tuwenming/models/THUdyh/Ola-7b" \
+    --image_path "./data/vision_qa_image.jpg"\
+    --audio_path "./data/vision_qa_audio.wav"
