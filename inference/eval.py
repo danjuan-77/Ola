c26d5b6a2f91d29514abcd6a3919688a2f0d70e2
danjuan-77 <youngertutu@gmail.com>
2025-04-29 16:31:56 +0800

    update

--- inference/eval.py
+++ inference/eval.py
@@ -616,6 +616,7 @@ if __name__ == "__main__":
     for data in tqdm(parsed_data):
         _id = data['id']
         _task = data['task']
+        _sub_task = data['subtask']
         text = data['text']
         audio_list = (
             [get_real_path(task_path, p) for p in data["audio_list"]]
@@ -666,6 +667,7 @@ if __name__ == "__main__":
         # print(f"output:>>>>>>>>{output}")
         pred_record = {
             "task": _task,
+            "sub_task": _sub_task,
             "id": _id,
             "predict": output,
         }
