a8f119edb216042bc4dc94fbaf924a2ce071e206
danjuan-77 <youngertutu@gmail.com>
2025-05-05 20:40:37 +0800

    update

--- eval_gpu1.sh
+++ eval_gpu1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
-python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_2/MVIC
+# python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_2/MVIC
 
-python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVH
+# python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVH
 
 python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVL
 
-python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVM
+# python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVM
 
 # nohup bash eval_gpu1.sh > /share/nlp/tuwenming/projects/HAVIB/logs/eval_gpu1_$(date +%Y%m%d%H%M%S).log 2>&1 &
