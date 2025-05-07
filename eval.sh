cfe14a446f6fb0150f14c2d049612cb8a87cb4b1
danjuan-77 <youngertutu@gmail.com>
2025-05-07 17:53:21 +0800

    update

--- eval.sh
+++ eval.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
 
+python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVL
+
 python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVLG
 
 python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_5/AVLG
