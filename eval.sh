a07185535ddd23ce1555e869a53ed4d82768aa01
danjuan-77 <youngertutu@gmail.com>
2025-05-07 17:37:26 +0800

    更新 eval.sh 脚本，修改 CUDA 设备为 0，更新评估任务路径，调整日志文件命名格式。

--- eval.sh
+++ eval.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=0
 
-python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVM
-
-python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVR
-
-python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVC
-
-python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVQA
+python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVLG
 
 python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_5/AVLG
 
-python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_5/AVQA
-
-# nohup bash eval.sh > /share/nlp/tuwenming/projects/HAVIB/logs/eval_ola_gpu3_$(date +%Y%m%d%H%M%S).log 2>&1 &
+# nohup bash eval.sh > /share/nlp/tuwenming/projects/HAVIB/logs/eval_gpu0_$(date +%Y%m%d%H%M%S).log 2>&1 &
