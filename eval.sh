6dcbd3adc913c21c42d3eec58bfabce3db3607d8
danjuan-77 <youngertutu@gmail.com>
2025-05-12 13:21:02 +0800

    update

--- eval.sh
+++ eval.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
 
-python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVL
+python data_test.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVH
 
-python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVLG
+python data_test.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/AVL
 
-python inference/eval.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_5/AVLG
+python data_test.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_3/VAH
 
-# nohup bash eval.sh > /share/nlp/tuwenming/projects/HAVIB/logs/eval_gpu0_$(date +%Y%m%d%H%M%S).log 2>&1 &
+python data_test.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVC
+
+python data_test.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVLG
+
+python data_test.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_4/AVQA
+
+python data_test.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_5/AVLG
+
+python data_test.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_5/AVQA
+
+# nohup bash eval.sh > /share/nlp/tuwenming/projects/HAVIB/logs/eval_ola_unimodal_gpu0_$(date +%Y%m%d%H%M%S).log 2>&1 &
